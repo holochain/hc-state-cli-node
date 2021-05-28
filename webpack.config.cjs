bffbec464b03baef875573c6b86fb52c657f52e5
jetttech <ldjetton21@gmail.com>
2021-05-28 06:15:58 -0500

    return to webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,21 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+
+  },
+  entry: ["./src/index.js"],
+  module: {
+    rules: [
+      { test: /\.js$/, include: /node_modules/, type: "javascript/auto" },
+    ],
+  },
+  target: 'node',
+  mode: "development",
+  plugins: [
+    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
+  ],
+};
