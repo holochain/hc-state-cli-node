a2ba9bf414bec4d30b095a86145443f2e0f8b0b4
jetttech <ldjetton21@gmail.com>
2021-05-28 04:47:03 -0500

    format remaining cmds with commander

--- src/index.js
+++ src/index.js
@@ -2,35 +2,6 @@ import { getAdminWebsocket, getAppWebsocket, listDnas, listCellIds, listActiveAp
 const { version } = require('../package.json');
 const { Command } = require('commander');
 
-const inputGuide = `
-  CLI tool for querying holochain over admin port (default = 4444)
-    usage:
-      hc-state --command arg
-
-      -a --list-active-app-ids (no arg) calls listActiveApps(void) -> [AppId: string]
-      -c --list-cell-ids (no arg) calls ListCellIds(void) -> [CellId: CellIdBase64]
-      -d --list-dnas (no arg) calls ListDnas(void) -> [DnaHash: string]
-      -s --state-dump CellIdBase64 calls dumpState(CellIdBase64) -> [stateDump: any]
-      -i --app-info InstalledAppId calls appInfo(InstalledAppId) -> { installed_app_id: string, cell_data: [{cell_id: CellIdBase64, cell_nick: string}], active: boolean}
-      -p --app-port 
-      -m --admin-port
-      -h --help shows this help
-
-    where
-      CellIdBase64 =
-        [
-          DnaHashBase64: string, // base64 representation of Buffer length 39
-          AgentPubKeyBase64: string // base64 representation of Buffer length 39
-        ]
-      or numeric index of cell ID returned by ListCellIds
-
-    example
-      hc-state -s "[
-        'hC0kqcfqvJ8krBR0bNnPsmLtFiEiMOHM0fX+U8FW+ROc7P10tUdc',
-        'hCAkcIRv7RZNVg8FWc6/oJZo04dZTXm7JP6tfMk3RptPY02cBQac'
-      ]"
-  `;
-
 const call_admin_port = async (async_fn, port) => {
   console.log("Invoking call to Admin-Interface on port : ", port);
   const adminWebsocket = await getAdminWebsocket(port)
@@ -57,22 +28,80 @@ export async function getArgs() {
 
   program
     .version(version)
+    .description('CLI tool for querying holochain over admin port (default = 4444)')
     .option('-p, --app-port <port>', 'assigns app port for outbound app-interface calls', 42233 )
     .option('-m, --admin-port <port>', 'assigns admin port for outbond admin-interface calls', 4444 )
     // .addHelpText('afterAll', inputGuide)
     
-    program
+  program
     .command("listDnas")
     .alias('d')
-    .description("lists installed DNAs")
+    .description("list installed DNAs: calls ListDnas(void) -> [DnaHash: string]")
     .action(async (cmd) => {
       // console.log('listDnas cmd %s', cmd);
       const result = await call_admin_port(listDnas, program.opts().adminPort);
       console.log(`Installed DNAs:`);
       console.log(result);
     })
-  
-    program.parse( process.argv );
+    
+  program
+    .command("listCellIds")
+    .alias('c')
+    .description("list installed cells IDs: calls listCellIds(void) -> [CellId: CellIdBase64]")
+    .action(async (cmd) => {
+      // console.log('listCellIds cmd %s', cmd);
+      const result = await call_admin_port(listCellIds, program.opts().adminPort);
+      console.log(`Installed CellIds:`);
+      console.log(result);
+    })
+    .addHelpCommand('after', `where
+    CellIdBase64 =
+      [
+        DnaHashBase64: string, // base64 representation of Buffer length 39
+        AgentPubKeyBase64: string // base64 representation of Buffer length 39
+      ]
+      or numeric index of cell ID returned by ListCellIds
+
+    example
+      hc-state -s "[
+        'hC0kqcfqvJ8krBR0bNnPsmLtFiEiMOHM0fX+U8FW+ROc7P10tUdc',
+        'hCAkcIRv7RZNVg8FWc6/oJZo04dZTXm7JP6tfMk3RptPY02cBQac'
+      ]"
+    `)
+
+  program
+    .command("listActiveApps")
+    .alias('a')
+    .description("list active app IDs: calls listActiveApps(void) -> [AppId: string]")
+    .action(async (cmd) => {
+      // console.log('listActiveApps cmd %s', cmd);
+      const result = await call_admin_port(listActiveApps, program.opts().adminPort);
+      console.log(`Active App IDs:`);
+      console.log(result);
+    })
+
+  program
+    .command("stateDump")
+    .alias('s')
+    .description("dump chain state for app: calls dumpState(CellIdBase64) -> [stateDump: any]")
+    .action(async (cmd) => {
+      const result = await call_admin_port(dumpState, program.opts().adminPort);
+      console.log(`State Dump for App:`);
+      console.log(result);
+    })  
+
+  program
+    .command("appInfo")
+    .alias('i')
+    .description("print app info for app: calls appInfo(InstalledAppId) -> { installed_app_id: string, cell_data: [{cell_id: CellIdBase64, cell_nick: string}], active: boolean}")
+    .action(async (cmd) => {
+      // console.log('appInfo cmd %s', cmd);
+      const result = await call_app_port(appInfo, program.opts().adminPort);
+      console.log(`App Info for App:`);
+      console.log(result);
+    })
+    
+  await program.parseAsync( process.argv );
 }
 
 try {
