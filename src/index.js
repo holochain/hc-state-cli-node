4b3db923aac7d94742aab7685553ec632f5601ab
jetttech <ldjetton21@gmail.com>
2021-05-28 06:02:34 -0500

    add clarity to cmd descriptions

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import { getAdminWebsocket, getAppWebsocket, listDnas, listCellIds, listActiveAp
 const { version } = require('../package.json');
 const { Command } = require('commander');
 
-const call_admin_port = async (async_fn, port) => {
-  console.log("Invoking call to Admin-Interface on port : ", port);
+const call_admin_port = async (async_fn, port, args) => {
+  const argsLog = args ? args.toString() : 'none';
+  console.log('Invoking call to Admin-Interface on port (%s) with args (%s)', port, argsLog);
   const adminWebsocket = await getAdminWebsocket(port)
   console.log('admin ws : ', adminWebsocket)
   try {
-    return await async_fn(adminWebsocket)
+    return await async_fn(adminWebsocket, args)
   } catch (error) {
     throw new Error(error)
   }
 }
 
-const call_app_port = async (async_fn, port) => {
-  console.log("Invoking call to App-Interface on port : ", port);
+const call_app_port = async (async_fn, port, args) => {
+  const argsLog = args ? args.toString() : 'none';
+  console.log('Invoking call to App-Interface on port (%s) with args (%s) ', port, argsLog);
   const appWebsocket = await getAppWebsocket(port)
   try {
-    return await async_fn(appWebsocket)
+    return await async_fn(appWebsocket, args)
   } catch (error) {
     throw new Error(error)
   }
@@ -35,13 +37,14 @@ export async function getArgs() {
         DnaHashBase64: string, // base64 representation of Buffer length 39
         AgentPubKeyBase64: string // base64 representation of Buffer length 39
       ]
-      or numeric index of cell ID returned by ListCellIds
     
   example
-    hc-state -s "[
-      'hC0kqcfqvJ8krBR0bNnPsmLtFiEiMOHM0fX+U8FW+ROc7P10tUdc',
-      'hCAkcIRv7RZNVg8FWc6/oJZo04dZTXm7JP6tfMk3RptPY02cBQac'
-    ]`)
+    hc-state -s "['hC0kqcfqvJ8krBR0bNnPsmLtFiEiMOHM0fX+U8FW+ROc7P10tUdc','hCAkcIRv7RZNVg8FWc6/oJZo04dZTXm7JP6tfMk3RptPY02cBQac'] 
+    // arg is the CellIdBase64 as a continuous string (should not contain spaces)
+    or 
+    hc-state -s 0 
+    // arg is the numeric index of cell ID returned by ListCellIds
+  `)
     .option('-p, --app-port <port>', 'assigns app port for outbound app-interface calls', 42233 )
     .option('-m, --admin-port <port>', 'assigns admin port for outbond admin-interface calls', 4444 )
     
@@ -76,23 +79,22 @@ export async function getArgs() {
     })
 
   program
-    .command("stateDump")
+    .command("stateDump <CellIdBase64 | Index>")
     .alias('s')
-    .description("dump chain state for app: calls dumpState(CellIdBase64) -> [stateDump: any]")
-    .action(async (cmd) => {
-      // console.log(' 1 cmd %s', cmd);
-      const result = await call_admin_port(dumpState, program.opts().adminPort);
+    .description("dump chain state for app: calls dumpState(CellIdBase64 | Index) -> [stateDump: any]")
+    .action(async (CellIdBase64) => {
+      console.log('CellIdBase64 %s', CellIdBase64);
+      const result = await call_admin_port(dumpState, program.opts().adminPort, CellIdBase64);
       console.log(`State Dump for App:`);
       console.log(result);
     })  
 
   program
-    .command("appInfo")
+    .command("appInfo <InstalledAppId>")
     .alias('i')
     .description("print app info for app: calls appInfo(InstalledAppId) -> { installed_app_id: string, cell_data: [{cell_id: CellIdBase64, cell_nick: string}], active: boolean}")
-    .action(async (cmd) => {
-      // console.log(' 2 cmd %s', cmd);
-      const result = await call_app_port(appInfo, program.opts().adminPort);
+    .action(async (installedAppId) => { 
+      const result = await call_app_port(appInfo, program.opts().appPort, installedAppId);
       console.log(`App Info for App:`);
       console.log(result);
     })
