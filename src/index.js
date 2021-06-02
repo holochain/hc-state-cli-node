559bed0cc717ba5c70effa676423217bd7a79606
jetttech <ldjetton21@gmail.com>
2021-06-02 18:26:20 -0500

    add standard x

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { getAdminWebsocket, getAppWebsocket, listDnas, listCellIds, listActiveApps, dumpState, appInfo } from "./utils";
-const { version } = require('../package.json');
-const { Command } = require('commander');
+import { getAdminWebsocket, getAppWebsocket, listDnas, listCellIds, listActiveApps, dumpState, appInfo } from './utils'
+const { version } = require('../package.json')
+const { Command } = require('commander')
 
 const call_admin_port = async (async_fn, port, args) => {
-  const argsLog = args ? args.toString() : 'none';
-  console.log('Invoking call to Admin-Interface on port (%s) with args (%s)', port, argsLog);
-  const adminWebsocket = await getAdminWebsocket(port)
-  console.log('admin ws : ', adminWebsocket)
+  const argsLog = args ? args.toString() : 'none'
   try {
+    console.log('Invoking call to Admin-Interface on port (%s) with args (%s)', port, argsLog)
+    const adminWebsocket = await getAdminWebsocket(port)
+    console.log('admin ws : ', adminWebsocket)
     return await async_fn(adminWebsocket, args)
   } catch (error) {
     throw new Error(error)
@@ -15,18 +15,18 @@ const call_admin_port = async (async_fn, port, args) => {
 }
 
 const call_app_port = async (async_fn, port, args) => {
-  const argsLog = args ? args.toString() : 'none';
-  console.log('Invoking call to App-Interface on port (%s) with args (%s) ', port, argsLog);
-  const appWebsocket = await getAppWebsocket(port)
+  const argsLog = args ? args.toString() : 'none'
   try {
+    console.log('Invoking call to App-Interface on port (%s) with args (%s) ', port, argsLog)
+    const appWebsocket = await getAppWebsocket(port)
     return await async_fn(appWebsocket, args)
   } catch (error) {
     throw new Error(error)
   }
 }
 
-export async function getArgs() {
-  const program = new Command();
+export async function getArgs () {
+  const program = new Command()
 
   program
     .version(version)
@@ -45,68 +45,68 @@ export async function getArgs() {
     hc-state -s 0 
     // arg is the numeric index of cell ID returned by ListCellIds
   `)
-    .option('-p, --app-port <port>', 'assigns app port for outbound app-interface calls', 42233 )
-    .option('-m, --admin-port <port>', 'assigns admin port for outbond admin-interface calls', 4444 )
-    
+    .option('-p, --app-port <port>', 'assigns app port for outbound app-interface calls', 42233)
+    .option('-m, --admin-port <port>', 'assigns admin port for outbond admin-interface calls', 4444)
+
   program
-    .command("listDnas")
+    .command('listDnas')
     .alias('d')
-    .description("list installed DNAs: calls ListDnas(void) -> [DnaHash: string]")
+    .description('list installed DNAs: calls ListDnas(void) -> [DnaHash: string]')
     .action(async () => {
-      const result = await call_admin_port(listDnas, program.opts().adminPort);
-      console.log(`Installed DNAs:`);
-      console.log(result);
+      const result = await call_admin_port(listDnas, program.opts().adminPort)
+      console.log('Installed DNAs:')
+      console.log(result)
     })
-    
+
   program
-    .command("listCellIds")
+    .command('listCellIds')
     .alias('c')
-    .description(`list installed cells IDs: calls listCellIds(void) -> [CellId: CellIdBase64]`)
+    .description('list installed cells IDs: calls listCellIds(void) -> [CellId: CellIdBase64]')
     .action(async () => {
-      const result = await call_admin_port(listCellIds, program.opts().adminPort);
-      console.log(`Installed CellIds:`);
-      console.log(result);
+      const result = await call_admin_port(listCellIds, program.opts().adminPort)
+      console.log('Installed CellIds:')
+      console.log(result)
     })
 
   program
-    .command("listActiveApps")
+    .command('listActiveApps')
     .alias('a')
-    .description("list active app IDs: calls listActiveApps(void) -> [AppId: string]")
+    .description('list active app IDs: calls listActiveApps(void) -> [AppId: string]')
     .action(async () => {
-      const result = await call_admin_port(listActiveApps, program.opts().adminPort);
-      console.log(`Active App IDs:`);
-      console.log(result);
+      const result = await call_admin_port(listActiveApps, program.opts().adminPort)
+      console.log('Active App IDs:')
+      console.log(result)
     })
 
   program
-    .command("stateDump <CellIdBase64 | Index>")
+    .command('stateDump <CellIdBase64 | Index>')
     .alias('s')
-    .description("dump chain state for app: calls dumpState(CellIdBase64 | Index) -> [stateDump: any]")
+    .description('dump chain state for app: calls dumpState(CellIdBase64 | Index) -> [stateDump: any]')
     .action(async (CellIdBase64) => {
-      console.log('CellIdBase64 %s', CellIdBase64);
-      const result = await call_admin_port(dumpState, program.opts().adminPort, CellIdBase64);
-      console.log(`State Dump for App:`);
-      console.log(result);
-    })  
+      console.log('CellIdBase64 %s', CellIdBase64)
+      const result = await call_admin_port(dumpState, program.opts().adminPort, CellIdBase64)
+      console.log('State Dump for App:')
+      console.log(result)
+    })
 
   program
-    .command("appInfo <InstalledAppId>")
+    .command('appInfo <InstalledAppId>')
     .alias('i')
-    .description("print app info for app: calls appInfo(InstalledAppId) -> { installed_app_id: string, cell_data: [{cell_id: CellIdBase64, cell_nick: string}], active: boolean}")
-    .action(async (installedAppId) => { 
-      const result = await call_app_port(appInfo, program.opts().appPort, installedAppId);
-      console.log(`App Info for App:`);
-      console.log(result);
+    .description('print app info for app: calls appInfo(InstalledAppId) -> { installed_app_id: string, cell_data: [{cell_id: CellIdBase64, cell_nick: string}], active: boolean}')
+    .action(async (installedAppId) => {
+      const result = await call_app_port(appInfo, program.opts().appPort, installedAppId)
+      console.log('App Info for App:')
+      console.log(result)
     })
-    
-  await program.parseAsync( process.argv );
+
+  await program.parseAsync(process.argv)
 }
 
 try {
   getArgs()
-    .then(()=> process.exit())
-    .catch((error) => { throw new Error(error) });
+    .then(() => process.exit())
+    .catch(e => { throw new Error(e) })
 } catch (e) {
-  console.error(e.message);
-  process.exit(1);
+  console.error(e.message)
+  process.exit(1)
 }
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,21 +1,21 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
 
   },
-  entry: ["./src/index.js"],
+  entry: ['./src/index.js'],
   module: {
     rules: [
-      { test: /\.js$/, include: /node_modules/, type: "javascript/auto" },
-    ],
+      { test: /\.js$/, include: /node_modules/, type: 'javascript/auto' }
+    ]
   },
   target: 'node',
-  mode: "development",
+  mode: 'development',
   plugins: [
-    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
-  ],
-};
+    new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })
+  ]
+}
